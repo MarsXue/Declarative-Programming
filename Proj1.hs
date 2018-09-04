347c1957c62bd7e499293a94b882549a3ebdac3e
Mars Xue <marsxue0221@gmail.com>
2018-09-04 13:26:57 +1000

    Final Version for Project 1 2018

--- Proj1.hs
+++ Proj1.hs
@@ -1,7 +1,7 @@
 --  File     : Proj1.hs
 --  Author   : Wenqing Xue (wenqingx)
 --  Origin   : Sat Aug 18 2018
---  Purpose  : Guess the correct lineup as less as possible
+--  Purpose  : Guess the correct selected lineup as less as possible
 
 module Proj1 (Person, parsePerson, height, hair, sex,
               GameState, initialGuess, nextGuess, feedback) where
@@ -47,6 +47,7 @@ parsePerson (h:c:g:[])
     | checkValid = Just (Person (read [h]) (read [c]) (read [g]))
     | otherwise = Nothing
     where
+        -- Check if all elements are valid
         checkValid = (h `elem` "ST") && (c `elem` "BRD") && (g `elem` "MF")
 
 -- Return the person's height
@@ -65,15 +66,21 @@ sex (Person _ _ g) = g
 expNum :: [[Person]] -> Int
 expNum ls = minIndex [calculation (frequency [feedback x y | y <- ls]) | x <- ls]
     where
+        -- Find the minimum index in list
         minIndex lst = head $ filter ((== minimum lst) . (lst !!)) [0..]
+        -- Calculate the frequency for each attribute
         frequency lst = map (\x -> length x) . group . sort $ lst
-        calculation lst = (fromIntegral (sum [x * x | x <- lst])) / (fromIntegral (sum lst))
+        -- Calculate the expected number by formula
+        calculation lst = (fromIntegral (sum [x * x | x <- lst])) /
+                          (fromIntegral (sum lst))
 
 -- Return the correct suspect, heights, hair colours and sexes in order
 feedback :: [Person] -> [Person] -> (Int, Int, Int, Int)
 feedback xs ys = (nPerson, nHeight, nHair, nSex)
     where
+        -- Find the match of each pair
         nPerson = match xs ys
+        -- Find the match of each pair, and delete the equal person number
         nHeight = match [height x | x <- xs] [height y | y <- ys] - nPerson
         nHair   = match [hair x | x <- xs] [hair y | y <- ys] - nPerson
         nSex    = match [sex x | x <- xs] [sex y | y <- ys] - nPerson
@@ -82,13 +89,19 @@ feedback xs ys = (nPerson, nHeight, nHair, nSex)
 initialGuess :: ([Person], GameState)
 initialGuess = (guess, GameState {guesses = allGuesses})
     where
+        -- Take the initial lineup with the lowest expected number
         guess = lineups !! (expNum lineups)
+        -- Take the all lineups except the current guess
         allGuesses = delete guess lineups
 
 -- Return the next lineup guess and new game state
-nextGuess :: ([Person], GameState) -> (Int, Int, Int, Int) -> ([Person], GameState)
+nextGuess :: ([Person], GameState) -> (Int, Int, Int, Int)
+                                   -> ([Person], GameState)
 nextGuess (oldGuess, gs) score = (newGuess, GameState {guesses = allGuesses})
     where
+        -- Take the lineups only match to the given feedback
         posGuesses = [x | x <- guesses gs, feedback oldGuess x == score]
+        -- Take the first lineup by calculated expected number
         newGuess = posGuesses !! (expNum posGuesses)
+        -- Take the rest of possible lineups for backup
         allGuesses = delete newGuess posGuesses
